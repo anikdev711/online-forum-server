4ff0b4b8785d0b7c36b9498a2ae07e9322a8fb11
anikdev711 <anikdev100@gmail.com>
2023-11-25 16:31:05 +0600

    create user in database added

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l2zfswc.mongodb.net/?retryWrites=true&w=majority`;
@@ -24,12 +24,50 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("forumDb").collection("users");
+
+
+
+    //user related
+
+    //post operation
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = {
+        email: user.email
+      }
+      const isUserExist = await userCollection.findOne(query);
+      if (isUserExist) {
+        return res.send({ message: 'user exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -41,9 +79,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Online Forum Running')
+  res.send('Online Forum Running')
 })
 
 app.listen(port, () => {
-    console.log(`Online forum is listening on port ${port}`)
+  console.log(`Online forum is listening on port ${port}`)
 })
