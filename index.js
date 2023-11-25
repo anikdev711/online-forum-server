668ade9ea6fd69d3251f7f47d25ce3e852c7d100
anikdev711 <anikdev100@gmail.com>
2023-11-26 01:36:53 +0600

    get operation for specific user added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,6 +30,23 @@ async function run() {
 
 
     //user related
+
+    //get all users
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    //get specific user
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = {
+        email: email
+      }
+      const result = await userCollection.findOne(query);
+      res.send(result)
+    })
 
     //post operation
     app.post('/users', async (req, res) => {
