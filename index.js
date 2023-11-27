1e26978d58b652b4a2d8860d1a84f9b21cc955cf
anikdev711 <anikdev100@gmail.com>
2023-11-28 01:24:52 +0600

    delete user operation added

--- index.js
+++ index.js
@@ -84,7 +84,14 @@ async function run() {
     })
 
     //delete user
-    
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
